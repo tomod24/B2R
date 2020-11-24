0643e9e8e864de5aab39276a2885f3cfc390cb8e
tomod24 <thomo_d24@hotmail.co.uk>
2020-11-24 10:59:16 +0000

    Workign connection to MONGODB issue with the connection unknown reasons but resolved due to testing and support as required.

--- mongo.py
+++ mongo.py
@@ -1,12 +1,13 @@
 import os
 import pymongo
-if os.path.exists("env.py"):
-    import env
+#if os.path.exists("env.py"):
+#    import env
 
 
-MONGO_URI = os.environ.get("MONGO_URI")
+MONGO_URI = "mongodb+srv://tomod24:Ludacris247@mybookdb.zflml.mongodb.net/books2review?retryWrites=true&w=majority"
 DATABASE = "books2review"
 COLLECTION = "books"
+
 
 
 def mongo_connect(url):
@@ -22,9 +23,9 @@ conn = mongo_connect(MONGO_URI)
 
 coll = conn[DATABASE][COLLECTION]
 
+coll.update_one({"title": "episode V"}, {"$set": {"title": "Episode V"}})
 
 documents = coll.find()
-
 
 for doc in documents:
     print(doc)

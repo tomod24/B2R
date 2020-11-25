bba3086cedf36d718aa912cfab9bc2e1c1fb1de7
tomod24 <thomo_d24@hotmail.co.uk>
2020-11-25 00:51:11 +0000

    add task input fields

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,12 @@
     $(".sidenav").sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        il8n: {
+            done: "Select"
+        }
+    });
   });
